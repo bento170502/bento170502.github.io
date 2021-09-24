37c29f6080370aae488397705d68931d35451eb5
bento170502 <88791367+bento170502@users.noreply.github.com>
2021-09-24 23:25:20 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Dear Jacques ;)',
+    introDesc: `I have a feeling \n
+    That I can't comprehend \n
+    In my deepest thoughts you are \n  
+    More than just a friend`,
+    btnIntro: 'click here 👋🏻',
+    title: 'Do you like meeeeeeehhh 🥺? ',
+    desc: 'you can try to press NO 😭.....if you can lol ',
+    btnYes: 'A big yes! 🥰',
+    btnNo: 'No! Are you dreaming?',
+    question:'There are 7 billions smiles in the world..Why mine is your favorite? 😛',
+    btnReply: 'Send it to Thành',
+    reply: 'genuine+cute+naughty..😈 lol ',
+    mess: 'Ye,I know 😂. You make me blush babe🙈 I like you too hehe ',
+    messDesc: 'What are you waiting for? Tell me about how your day was 🥰 ',
+    btnAccept: 'Lets gooo!!',
+    messLink: 'https://m.me/nguyen.tuanthanh17502/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
